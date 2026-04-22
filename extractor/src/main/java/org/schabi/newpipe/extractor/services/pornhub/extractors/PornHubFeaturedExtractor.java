b2481af59a920a1e511bc5a1524048c8aace9817
msiuuu <msiuuu@users.noreply.github.com>
2026-04-21 22:30:03 -0500

    Stub PornHub featured kiosk pending proper StreamInfoItem port

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/pornhub/extractors/PornHubFeaturedExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/pornhub/extractors/PornHubFeaturedExtractor.java
@@ -1,11 +1,16 @@
-// Created by Fynn Godau 2019, licensed GNU GPL version 3 or later
+// Forked from Fynn Godau's NewPipe Bandcamp extractor (2019), GNU GPL v3+.
+// Reworked for PornHub by msiuuu, 2026.
+//
+// Stubbed for v1. Trending kiosk requires switching the generic type from
+// PlaylistInfoItem to StreamInfoItem and updating service registration,
+// which is follow-up scope.
 
 package org.schabi.newpipe.extractor.services.pornhub.extractors;
 
-import com.grack.nanojson.JsonArray;
-import com.grack.nanojson.JsonObject;
-import com.grack.nanojson.JsonParser;
-import com.grack.nanojson.JsonParserException;
+import java.io.IOException;
+
+import javax.annotation.Nonnull;
+
 import org.schabi.newpipe.extractor.Page;
 import org.schabi.newpipe.extractor.StreamingService;
 import org.schabi.newpipe.extractor.downloader.Downloader;
@@ -16,40 +21,29 @@ import org.schabi.newpipe.extractor.linkhandler.ListLinkHandler;
 import org.schabi.newpipe.extractor.playlist.PlaylistInfoItem;
 import org.schabi.newpipe.extractor.playlist.PlaylistInfoItemsCollector;
 
-import javax.annotation.Nonnull;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-
-import static org.schabi.newpipe.extractor.services.pornhub.extractors.PornHubExtractorHelper.BASE_API_URL;
-
 public class PornHubFeaturedExtractor extends KioskExtractor<PlaylistInfoItem> {
 
     public static final String KIOSK_FEATURED = "Featured";
-    public static final String FEATURED_API_URL = BASE_API_URL + "/mobile/24/bootstrap_data";
-    public static final String MORE_FEATURED_API_URL
-            = BASE_API_URL + "/mobile/24/feed_older_logged_out";
 
-    private JsonObject json;
+    /**
+     * Placeholder URL identifying the Featured kiosk. References to this
+     * constant from the link handler factory + service class rely on the
+     * value existing; the eventual proper implementation will fetch this
+     * page. Not currently hit while the extractor is stubbed.
+     */
+    public static final String FEATURED_API_URL =
+            "https://www.pornhub.com/video?o=tr";
 
     public PornHubFeaturedExtractor(final StreamingService streamingService,
-                                     final ListLinkHandler listLinkHandler,
-                                     final String kioskId) {
+                                    final ListLinkHandler listLinkHandler,
+                                    final String kioskId) {
         super(streamingService, listLinkHandler, kioskId);
     }
 
     @Override
     public void onFetchPage(@Nonnull final Downloader downloader)
             throws IOException, ExtractionException {
-        try {
-            json = JsonParser.object().from(getDownloader().postWithContentTypeJson(
-                            FEATURED_API_URL,
-                            Collections.emptyMap(),
-                            "{\"platform\":\"\",\"version\":0}".getBytes(StandardCharsets.UTF_8))
-                    .responseBody());
-        } catch (final JsonParserException e) {
-            throw new ParsingException("Could not parse PornHub featured API response", e);
-        }
+        // no-op while stubbed
     }
 
     @Nonnull
@@ -62,57 +56,13 @@ public class PornHubFeaturedExtractor extends KioskExtractor<PlaylistInfoItem> {
     @Override
     public InfoItemsPage<PlaylistInfoItem> getInitialPage()
             throws IOException, ExtractionException {
-        final JsonArray featuredStories = json.getObject("feed_content")
-                .getObject("stories")
-                .getArray("featured");
-
-        return extractItems(featuredStories);
-    }
-
-    private InfoItemsPage<PlaylistInfoItem> extractItems(final JsonArray featuredStories) {
-        final PlaylistInfoItemsCollector c = new PlaylistInfoItemsCollector(getServiceId());
-
-        for (int i = 0; i < featuredStories.size(); i++) {
-            final JsonObject featuredStory = featuredStories.getObject(i);
-
-            if (featuredStory.isNull("album_title")) {
-                // Is not an album, ignore
-                continue;
-            }
-
-            c.commit(new PornHubPlaylistInfoItemFeaturedExtractor(featuredStory));
-        }
-
-        final JsonObject lastFeaturedStory = featuredStories.getObject(featuredStories.size() - 1);
-        return new InfoItemsPage<>(c, getNextPageFrom(lastFeaturedStory));
-    }
-
-    /**
-     * Next Page can be generated from metadata of last featured story
-     */
-    private Page getNextPageFrom(final JsonObject lastFeaturedStory) {
-        final long lastStoryDate = lastFeaturedStory.getLong("story_date");
-        final long lastStoryId = lastFeaturedStory.getLong("ntid");
-        final String lastStoryType = lastFeaturedStory.getString("story_type");
-        return new Page(
-                MORE_FEATURED_API_URL + "?story_groups=featured"
-                        + ':' + lastStoryDate + ':' + lastStoryType + ':' + lastStoryId
-        );
+        return new InfoItemsPage<>(
+                new PlaylistInfoItemsCollector(getServiceId()), null);
     }
 
     @Override
     public InfoItemsPage<PlaylistInfoItem> getPage(final Page page)
             throws IOException, ExtractionException {
-
-        final JsonObject response;
-        try {
-            response = JsonParser.object().from(
-                    getDownloader().get(page.getUrl()).responseBody()
-            );
-        } catch (final JsonParserException e) {
-            throw new ParsingException("Could not parse PornHub featured API response", e);
-        }
-
-        return extractItems(response.getObject("stories").getArray("featured"));
+        return null;
     }
 }
